4289e7f037e2210684408f4bdc969371e0d2530a
IceSnipperring <3339354363@qq.com>
2023-12-16 22:35:52 +0800

    利用ai，稍微完善了代码，并且使用了spotify作为音乐播放器

--- index.js
+++ index.js
@@ -2,10 +2,79 @@ var about = document.querySelector('.about');
 var skills = document.querySelector('.skills');
 var experience = document.querySelector('.experience');
 var my_fav_video = document.querySelector('.my_fav_video');
+var my_fav_music = document.querySelector('.my_fav_music');
 var left_arrow = document.querySelector(".fa-caret-square-left");
 var right_arrow = document.querySelector(".fa-caret-square-right");
-
 var allSections = document.querySelectorAll(".about, .skills, .experience");
+
+//本来想着动态加载的，但是最后动画十分卡顿，所以放弃了
+//var spotify_music_iframe = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4L49kKEqUUBd6pAW9kLG3g?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+
+
+//加载youtube的api
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+//控制youtue视频,new出来的对象是我的div块，api会自动替换其为iframe框架
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('my_fav_video', {
+        height: '400',
+        width: '94%',
+        videoId: 'Q4WboePedWU'
+    });
+}
+
+//播放
+function playVideo() {
+    try {
+        player.playVideo();
+    } catch (error) {
+        //如果加载不了，那么可能是api没有导入成，也就是说网络问题，那么直接报错
+        document.getElementById('my_fav_video').innerText = "抱歉，YouTube视频加载失败，可能是网络原因哦";
+        document.getElementById('my_fav_video').style.textAlign = "center"
+    }
+}
+
+//暂停
+function stopVideo() {
+    try {
+        player.pauseVideo();
+    } catch (error) {
+
+    }
+}
+//控制spotify的超时
+var iframeContainer = document.getElementById('my_fav_music');
+var myIframe = document.getElementById('iframe_spotify');
+
+// 设置超时时间（以毫秒为单位）
+var timeoutMilliseconds = 5000; // 5秒
+
+// 设置一个定时器，在超时时间后检查 iframe 是否加载完成
+var timeoutTimer = setTimeout(function () {
+    handleTimeout();
+}, timeoutMilliseconds);
+
+// 监听 iframe 加载完成事件
+myIframe.addEventListener('load', function () {
+    // 清除超时定时器
+    clearTimeout(timeoutTimer);
+});
+
+// 处理加载超时的函数
+function handleTimeout() {
+    // 在原来的 div 中写入"加载异常"的内容
+    iframeContainer.innerHTML = '抱歉，Spotify视频加载失败，可能是网络原因哦';
+}
+
+// 设置超时定时器
+const controller = new AbortController();
+const timeoutId = setTimeout(() => controller.abort(), timeoutMilliseconds);
+
 
 function showPopup() {
     document.getElementById("popup").style.transform = "scale(1)";
@@ -21,8 +90,11 @@ function pre() {
         toggleActiveClass(skills, about);
     } else if (experience.classList.contains("active")) {
         toggleActiveClass(experience, skills);
+    } else if (my_fav_music.classList.contains("active")) {
+        toggleActiveClass(my_fav_music, experience);
     } else if (my_fav_video.classList.contains("active")) {
-        toggleActiveClass(my_fav_video, experience);
+        stopVideo();
+        toggleActiveClass(my_fav_video, my_fav_music);
     }
 }
 /*向后翻页*/
@@ -32,7 +104,10 @@ function next() {
     } else if (skills.classList.contains("active")) {
         toggleActiveClass(skills, experience);
     } else if (experience.classList.contains("active")) {
-        toggleActiveClass(experience, my_fav_video);
+        toggleActiveClass(experience, my_fav_music);
+    } else if (my_fav_music.classList.contains("active")) {
+        playVideo();
+        toggleActiveClass(my_fav_music, my_fav_video);
     }
 }
 
